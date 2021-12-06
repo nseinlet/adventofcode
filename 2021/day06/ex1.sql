df24aecca3d292240e3517010b20ff5d7de15546
Nicolas Seinlet <nse@openerp.com>
2021-12-06 08:52:16 +0100

    day 06

---
+++ 2021/day06/ex1.sql
@@ -0,0 +1,38 @@
+WITH RECURSIVE vals AS (VALUES('1,4,1,1,1,1,1,1,1,4,3,1,1,3,5,1,5,3,2,1,1,2,3,1,1,5,3,1,5,1,1,2,1,2,1,1,3,1,5,1,1,1,3,1,1,1,1,1,1,4,5,3,1,1,1,1,1,1,2,1,1,1,1,4,4,4,1,1,1,1,5,1,2,4,1,1,4,1,2,1,1,1,2,1,5,1,1,1,3,4,1,1,1,3,2,1,1,1,4,1,1,1,5,1,1,4,1,1,2,1,4,1,1,1,3,1,1,1,1,1,3,1,3,1,1,2,1,4,1,1,1,1,3,1,1,1,1,1,1,2,1,3,1,1,1,1,4,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,5,1,1,1,2,2,1,1,3,5,1,1,1,1,3,1,3,3,1,1,1,1,3,5,2,1,1,1,1,5,1,1,1,1,1,1,1,2,1,2,1,1,1,2,1,1,1,1,1,2,1,1,1,1,1,5,1,4,3,3,1,3,4,1,1,1,1,1,1,1,1,1,1,4,3,5,1,1,1,1,1,1,1,1,1,1,1,1,1,5,2,1,4,1,1,1,1,1,1,1,1,1,1,1,1,1,5,1,1,1,1,1,1,1,1,2,1,4,4,1,1,1,1,1,1,1,5,1,1,2,5,1,1,4,1,3,1,1')),
+init as (
+    SELECT regexp_split_to_table(column1, ',') as fish,
+           row_number() over() as id
+      FROM vals
+),
+reproduction AS (
+    SELECT fish::int as fish,
+           count(*) as nbr,
+           0 as turn
+      FROM init
+  GROUP BY fish
+ UNION ALL
+    (WITH fishes AS (
+      SELECT CASE fish
+               WHEN 0 THEN 6
+               WHEN -9 THEN 6
+               ELSE fish-1
+             END as fish,
+             nbr,
+             turn+1 as turn
+        FROM reproduction
+       WHERE turn<80
+    )
+    SELECT fish,
+           sum(nbr)::bigint,
+           turn
+      FROM fishes
+  GROUP BY fish,
+           turn
+ UNION ALL
+    SELECT -1 as fish,
+           nbr,
+           turn
+      FROM fishes
+     WHERE fish=6)
+)
+SELECT sum(nbr), turn FROM reproduction GROUP BY turn order by turn;
